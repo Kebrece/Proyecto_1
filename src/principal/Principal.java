8aba6f142e64bffdc8d16e66f459c327107666f5
Kebrece <72960066+Kebrece@users.noreply.github.com>
2021-09-14 06:58:15 -0600

    Tres saltos de linea 2.0

--- src/principal/Principal.java
+++ src/principal/Principal.java
@@ -3,7 +3,9 @@ package principal;
 public class Principal {
 public static void main(String [] args)
 {
-  System.out.println("Kevin Brenes");
+  System.out.println("Kevin");
+  System.out.println("Brenes");
+  System.out.println("Cerdas");
 }
 }
 
